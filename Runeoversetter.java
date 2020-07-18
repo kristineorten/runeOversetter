f54782fa963620bb3fc5658d2749a0484b5ec4ef
Kristine Orten <kristine.orten@gmail.com>
2020-07-18 21:32:23 +0200

    Initial upload

---
+++ Runeoversetter.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class Runeoversetter {
+    public static void main(String[] args) {
+        boolean filFunnet = false;
+
+        // Lager scanner for lesing av fil
+        String filnavn = "middelalderruner.txt";
+        Scanner lesFil = new Scanner(System.in);
+
+        try {
+            lesFil = new Scanner(new File(filnavn));
+            filFunnet = true;
+        } catch (FileNotFoundException e) {
+            System.out.println("Fant ikke filen \"" + filnavn + "\".");
+        }
+
+        // Går gjennom input fra bruker
+        if (filFunnet) {
+            System.out.println("Skriv inn teksten du vil oversette til runer:");
+
+            Scanner lesInput = new Scanner(System.in);
+            String input = lesInput.nextLine();
+            char[] bokstaver = input.toCharArray();
+            String runer = "";
+
+            for (int i = 0; i < bokstaver.length; i++) {
+                String denne = Character.toString(bokstaver[i]);
+                String neste = "";
+                if ((i+1) < bokstaver.length) {
+                    neste = Character.toString(bokstaver[i+1]);
+                }
+
+                if (erTall(denne)) {
+                    runer += denne;
+                } else {
+                    runer += oversett(denne, neste, lesFil);
+                }
+            }
+
+            System.out.println("Ferdig! Her er resultatet:");
+            System.out.println(runer);
+        }
+    }
+
+    public static String oversett(String denne, String neste, Scanner lesFil) {
+        String rune = "?";
+
+        while (lesFil.hasNext()) {
+            String linje = lesFil.nextLine();
+            String[] linjebiter = linje.split(" ");
+            String latinskBokstav = linjebiter[0];
+            String runeBokstav = linjebiter[1];
+
+            if (denne.equals(latinskBokstav)) {
+                String nesteRune = lesFil.nextLine().split(" ")[1];
+                if ((denne += neste).equals(nesteRune)) {
+                    rune = nesteRune;
+                } else {
+                    rune = runeBokstav;
+                }
+                return rune;
+            }
+        }
+
+        return rune;
+    }
+
+    public static boolean erTall(String i) {
+        try {
+            int tall = Integer.parseInt(i);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
